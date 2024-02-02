bc58a637a5c572b5cb73002be8f55812577421da
Ethann0s <135250333+Ethann0s@users.noreply.github.com>
2024-02-02 18:07:34 +0100

    Commit à la main parce que Julien avait pas git 3/3

--- pieces/Piece.py
+++ pieces/Piece.py
@@ -4,12 +4,14 @@ from config import board_origin, tile_size
 
 
 class Piece:
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, is_white: bool):
         self.x = x
         self.y = y
-        self.color = color
+        self.color = "#ffffff" if is_white else "#000000" 
 
     def go_to(self):
+        t.penup()
+        t.setheading(0)
         t.goto(board_origin + self.x * tile_size + tile_size / 2, board_origin + self.y * tile_size  + tile_size / 2)
         #on place au milieu de la case
 
