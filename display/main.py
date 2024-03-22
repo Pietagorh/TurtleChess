f12ca7e295ca6eda58147fa45c24921c8d400c1e
Pietagorh <pietagorhh@proton.me>
2024-03-22 16:22:50 +0100

    Update main.py

--- display/main.py
+++ display/main.py
@@ -1,6 +1,6 @@
 import turtle
-from display.config import tile_size
-from display.Board import Board
+from config import tile_size
+from Board import Board
 from bitarray import bitarray
 
 
@@ -13,10 +13,9 @@ def setup_turtle():
     turtle.screensize((tile_size + 1) * 8, (tile_size + 1) * 8, "#000000")
     turtle.setup((tile_size + 1) * 8 + 10, (tile_size + 1) * 8 + 10, 0, 0)
 
-
 setup_turtle()
 board = Board()
 board.draw_background()
 board.convert_from_bitarray(bitarray("1001001000000000000000000000000000000000000000000000000000000000"))
-
+board.pieces[0].draw()
 turtle.mainloop()
